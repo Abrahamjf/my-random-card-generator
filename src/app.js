3dd70bdc690f35eda1bce583002f0b2065d15e6f
Abrahamjf <abrahamjferrer@gmail.com>
2022-09-25 19:05:49 +0000

    :hammer: Se completo el proyecto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomSuit = generateRandomSuit();
+
+  document.querySelector(".top-suit").textContent = randomSuit;
+  document.querySelector(".bottom-suit").textContent = randomSuit;
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let index = Math.floor(Math.random() * numbers.length);
+  return numbers[index];
+};
+let generateRandomSuit = () => {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let i = Math.floor(Math.random() * suits.length);
+  return suits[i];
 };
